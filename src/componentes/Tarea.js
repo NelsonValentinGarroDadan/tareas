cf0f724e20855fb4c28d09ad36d570af190701ca
ConsultorioEducacionEspecial <154619788+ConsultorioEducacionEspecial@users.noreply.github.com>
2024-01-16 13:40:06 -0300

    initial commit

---
+++ src/componentes/Tarea.js
@@ -0,0 +1,22 @@
+import React from "react";
+import TareaCss from '../Hojas-Estilo/Tarea.css'
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+function Tarea({id, texto , completada, tareaCompletada, eliminarTarea}){
+    return(
+        <div className={ completada? 'tarea-contenedor completada': 'tarea-contenedor'}>
+            <div 
+                className="tarea-texto"
+                onClick={ () => tareaCompletada(id) }>
+                {texto}
+            </div>
+            <div 
+                className="tarea-contenedor-iconos"
+                onClick={ () => eliminarTarea(id) }>
+                <AiOutlineCloseCircle className="tarea-icono"/>
+            </div>
+        </div>
+    );
+};
+
+export default Tarea;
